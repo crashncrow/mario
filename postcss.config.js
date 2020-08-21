ddf55b684cd95c2ecd687f02c9879e9cc85168ce
Diego Gennaro <kdc.crow@gmail.com>
2020-08-20 23:24:53 -0300

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: [
+      './components/**/*.tsx',
+      './components/**/*.js',
+      './pages/**/*.tsx',
+      './pages/**/*.js',
+    ],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+]
+module.exports = {
+  plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+}
