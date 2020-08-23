b45483b2ba95757e3c34efe1ce82c0e893e595ed
Diego Gennaro <kdc.crow@gmail.com>
2020-08-23 01:27:17 -0300

    reduce dom elements + prugecss

--- postcss.config.js
+++ postcss.config.js
@@ -13,6 +13,6 @@ module.exports = {
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [purgecss]),
   ],
 }
