e58754a77a1275a004d4a888ff55f523dc205a57
Diego Gennaro <kdc.crow@gmail.com>
2026-02-25 17:46:12 -0300

    refactoring

--- components/Castle/index.js
+++ components/Castle/index.js
@@ -9,25 +9,25 @@ const Castle = ({ x }) => {
   const { renderLimit, pixels } = useAppContext()
 
   const base = [
-    { x: 205, y: 1, border: 0 },
-    { x: 206, y: 1, border: 0  },
-    { x: 208, y: 1, border: 0  },
-    { x: 209, y: 1, border: 0  },
+    { x: 205, y: 1 },
+    { x: 206, y: 1 },
+    { x: 208, y: 1 },
+    { x: 209, y: 1 },
 
-    { x: 205, y: 2, border: 0  },
-    { x: 206, y: 2, border: 0  },
-    { x: 207, y: 2, border: 0  },
-    { x: 208, y: 2, border: 0 },
-    { x: 209, y: 2, border: 0 },
+    { x: 205, y: 2 },
+    { x: 206, y: 2 },
+    { x: 207, y: 2 },
+    { x: 208, y: 2 },
+    { x: 209, y: 2 },
 
-    { x: 206, y: 4, border: 0 },
-    { x: 207, y: 4, border: 0 },
-    { x: 208, y: 4, border: 0 },
+    { x: 206, y: 4 },
+    { x: 207, y: 4 },
+    { x: 208, y: 4 },
   ]
 
   const roof = [
     { x: 205, y: 3 },
-    { x: 206, y: 3, bg: 1},
+    { x: 206, y: 3, bg: 1 },
     { x: 207, y: 3, bg: 1 },
     { x: 208, y: 3, bg: 1 },
     { x: 209, y: 3 },
@@ -46,7 +46,7 @@ const Castle = ({ x }) => {
     <>
       {
         base.filter(el => el.x * pixels < renderLimit).map((el, i) => (
-          <Brick key={`castle-brick-${el.x}-${el.y}-${i}`} x={el.x} y={el.y} touches={0} border={el.border} />
+          <Brick key={`castle-brick-${el.x}-${el.y}-${i}`} x={el.x} y={el.y} touches={0} border={0} />
         ))
       }
 
