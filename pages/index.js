1df38f2e89a484a971ff504522b26a9b05b948ba
Diego Gennaro <kdc.crow@gmail.com>
2026-02-25 23:47:13 -0300

    new hooks

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { useCallback, useEffect, useRef } from 'react'
 import { elements, pipes } from 'libs/elements'
 import { useAppContext } from 'contexts/AppContext'
-import useIsomorphicLayoutEffect from 'hooks/useIsomorphicLayoutEffect'
 import useDebugMetrics from 'hooks/useDebugMetrics'
 import useVisibleWorldWindow from 'hooks/useVisibleWorldWindow'
 import useGameInput from 'hooks/useGameInput'
+import useCameraFollow from 'hooks/useCameraFollow'
 
 import Head from 'next/head'
 import Sky from 'components/Sky'
@@ -65,38 +65,13 @@ export default function Home() {
     setObjects(elements)
   }, [setObjects])
 
-  useIsomorphicLayoutEffect(() => {
-    if (!gameLoopEnabled || !worldRef.current) return
-
-    const currentCameraX = cameraXRef.current
-    const screenX = left - currentCameraX
-    const deadZoneLeft = 88
-    const deadZoneRight = 112
-
-    let nextCameraX = currentCameraX
-
-    if (screenX < deadZoneLeft) {
-      nextCameraX = left - deadZoneLeft
-    } else if (screenX > deadZoneRight) {
-      nextCameraX = left - deadZoneRight
-    }
-
-    nextCameraX = Math.max(0, Math.round(nextCameraX))
-    nextCameraX = Math.min(nextCameraX, maxCameraX)
-
-    if (Math.abs(currentCameraX - nextCameraX) > 0) {
-      cameraXRef.current = nextCameraX
-      worldRef.current.style.transform = `translate3d(${-nextCameraX}px, 0, 0)`
-    }
-  }, [gameLoopEnabled, left, maxCameraX])
-
-  useEffect(() => {
-    if (gameLoopEnabled) return
-    cameraXRef.current = 0
-    if (worldRef.current) {
-      worldRef.current.style.transform = ''
-    }
-  }, [gameLoopEnabled])
+  useCameraFollow({
+    gameLoopEnabled,
+    worldRef,
+    cameraXRef,
+    left,
+    maxCameraX,
+  })
 
   const jump = useCallback((limit) => {
     let j = 0
