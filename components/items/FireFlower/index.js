0b163bf3619239f5414944c1201ebeb78e3a1efc
Diego Gennaro <kdc.crow@gmail.com>
2026-03-01 15:06:57 -0300

    fire flower

--- components/items/FireFlower/index.js
+++ components/items/FireFlower/index.js
@@ -1,10 +1,27 @@
 const FireFlower = () => (
-  <div className='relative w-8 h-10'>
-    <div className='absolute left-3 top-5 w-2 h-5 bg-green-light'></div>
-    <div className='absolute left-0 top-2 w-3 h-3 rounded-full bg-red-500'></div>
-    <div className='absolute left-5 top-2 w-3 h-3 rounded-full bg-red-500'></div>
-    <div className='absolute left-2 top-0 w-4 h-4 rounded-full bg-red-500'></div>
-    <div className='absolute left-2 top-3 w-4 h-4 rounded-full bg-yellow-300'></div>
+  <div className='w-16 h-16'>
+    <div className="w-full">
+      <div className='absolute left-4 bottom-15 w-8 h-1 bg-white'></div>
+      <div className='absolute left-2 bottom-14 w-12 h-1 bg-white'></div>
+      <div className='absolute left-1 bottom-13 w-14 h-1 bg-white'></div>
+      <div className='absolute left-0 bottom-11 w-16 h-2 bg-white'></div>
+      <div className='absolute left-1 bottom-10 w-14 h-1 bg-white'></div>
+      <div className='absolute left-2 bottom-9 w-12 h-1 bg-white'></div>
+      <div className='absolute left-4 bottom-8 w-8 h-1 bg-white'></div>
+      <div className="absolute left-4 bottom-10 w-8 h-4 bg-green-dark border-4 border-box-orange"></div>
+      <div className="absolute left-2 bottom-11 w-12 h-2 border-x-8 border-box-orange"></div>
+    </div>
+
+    <div className='absolute left-7 bottom-0 w-2 h-8 bg-green-dark'></div>
+
+    <div className='absolute left-0 bottom-6 w-full h-1 border-x-10 border-green-dark'></div>
+    <div className='absolute left-1 bottom-5 w-14 h-1 border-x-10 border-green-dark'></div>
+    <div className='absolute left-1 bottom-4 w-14 h-1 border-x-13 border-green-dark'></div>
+    <div className='absolute left-2 bottom-2 w-12 h-2 border-x-14 border-green-dark'></div>
+    
+    <div className='absolute left-3 bottom-1 w-10 h-1 bg-green-dark'></div>
+
+    <div className='absolute left-6 bottom-0 w-4 h-2 bg-green-dark'></div>
   </div>
 )
 
